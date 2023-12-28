faa92ce0fbd343b9c197fe0b22884feb6b8928f6
Triavanicus <triavanicus@gmail.com>
2023-12-28 10:13:59 -0600

    Used prettier to format the code

--- 2023/javascript/d2.js
+++ 2023/javascript/d2.js
@@ -3,24 +3,29 @@ import { read, run } from "./utils.js";
 function day2() {
   const data = read("./d2.txt");
 
-  let result = data.split("\n")
-    .filter(s => s != "")
+  let result = data
+    .split("\n")
+    .filter((s) => s != "")
     .map((line) => {
       let maxCubes = {};
       const gameInfo = line.split(":");
       const gameNum = gameInfo[0].split(" ")[1].trim();
-      const gameTurns = gameInfo[1].split(";")
-        .flatMap(s => s.split(","))
-        .map(s => s.trim());
+      const gameTurns = gameInfo[1]
+        .split(";")
+        .flatMap((s) => s.split(","))
+        .map((s) => s.trim());
 
       gameTurns.forEach((turn) => {
         const info = turn.split(" ");
         maxCubes[info[1]] = Math.max(maxCubes[info[1]] || 0, parseInt(info[0]));
       });
 
-      return [parseInt(gameNum), maxCubes]
-    }).reduce((acc, game) => acc + Object.values(game[1])
-      .reduce((acc, n) => n * acc), 0);
+      return [parseInt(gameNum), maxCubes];
+    })
+    .reduce(
+      (acc, game) => acc + Object.values(game[1]).reduce((acc, n) => n * acc),
+      0,
+    );
 
   return result;
 }
--- 2023/javascript/d3.js
+++ 2023/javascript/d3.js
@@ -4,7 +4,7 @@ function day3() {
   let data = read("d3.txt");
   const lineLength = data.indexOf("\n");
   data = data.split("\n").join("");
-  const notSymbols = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+  const notSymbols = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
   const adjacent = [
     -1 - lineLength,
     -lineLength,
@@ -13,32 +13,55 @@ function day3() {
     1,
     lineLength - 1,
     lineLength,
-    lineLength + 1
+    lineLength + 1,
   ];
-  return data.split("")
+
+  return data
+    .split("")
     .reduce((symbols, char, index) => {
       if (notSymbols.indexOf(char) === -1) {
         symbols.push(index);
       }
       return symbols;
-    }, []).reduce((acc, i) => {
-      acc.push(adjacent.filter((n) => i + n >= 0 && i + n < data.length)
-        .map((n) => i + n)
-        .filter((n) => notSymbols.indexOf(data[n]) >= 0
-          && notSymbols.indexOf(data[n]) < 10));
+    }, [])
+    .reduce((acc, i) => {
+      acc.push(
+        adjacent
+          .filter((n) => i + n >= 0 && i + n < data.length)
+          .map((n) => i + n)
+          .filter(
+            (n) =>
+              notSymbols.indexOf(data[n]) >= 0 &&
+              notSymbols.indexOf(data[n]) < 10,
+          ),
+      );
       return acc;
-    }, []).flat().sort((a, b) => a - b).filter((n, i, arr) => {
-      console.log(n);
-      if (i + 1 >= arr.length || arr[i + 1] !== n + 1 && arr[i + 1] !== n) return true;
+    }, [])
+    .flat()
+    .sort((a, b) => a - b)
+    .filter((n, i, arr) => {
+      if (i + 1 >= arr.length || (arr[i + 1] !== n + 1 && arr[i + 1] !== n))
+        return true;
       return false;
-    }).map((n) => {
+    })
+    .map((n) => {
       let min = n;
       let max = n;
-      for (; notSymbols.indexOf(data[min - 1]) !== -1 && notSymbols.indexOf(data[min - 1]) < 10; min--);
-      for (; notSymbols.indexOf(data[max]) !== -1 && notSymbols.indexOf(data[max]) < 10; max++);
-      //console.log(+data.slice(min,max));
+      for (
+        ;
+        notSymbols.indexOf(data[min - 1]) !== -1 &&
+        notSymbols.indexOf(data[min - 1]) < 10;
+        min--
+      );
+      for (
+        ;
+        notSymbols.indexOf(data[max]) !== -1 &&
+        notSymbols.indexOf(data[max]) < 10;
+        max++
+      );
       return +data.slice(min, max);
-    }).reduce((acc, n) => acc + n);
+    })
+    .reduce((acc, n) => acc + n);
 }
 
 run(day3);
